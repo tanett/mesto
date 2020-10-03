dc08def40f47ff29786299a4067fa90cf69c3e12
Maxim <samai2@yandex.ru>
2020-10-03 19:14:42 +0300

    переделано закрытие

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,10 +3,44 @@ const buttonClosePopup = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 const form = document.querySelector('.popup__container');
 
-
-function popupToggle() {
-    popup.classList.toggle('popup_opened')
+function popupOpen() {
+    popup.classList.add('popup_opened');
 }
+
+function popupClose() {
+    popup.classList.remove('popup_opened');
+}
+
+function onMissclickPopup(event) {
+    if (event.target !== event.currentTarget) {
+        return;
+    }
+    popupClose();
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    let nameInput = document.querySelector('.popup__input-name');
+    let jobInput = document.querySelector('.popup__input-about');
+    console.log(nameInput.value);
+    console.log(jobInput.value);
+    let profileName = document.querySelector('.profile__name');
+    let profileDescr = document.querySelector('.profile__descr');
+
+    profileName.textContent = nameInput.value;
+    profileDescr.textContent = jobInput.value;
+    popupClose();
+}
+form.addEventListener('submit', formSubmitHandler);
+popup.addEventListener('click', onMissclickPopup);
+buttonOpenPopup.addEventListener('click', popupOpen);
+buttonClosePopup.addEventListener('click', popupClose);
+
+
+/*function popupToggle() {
+    popup.classList.toggle('popup_opened');
+}
+
 
 function onMissclickPopup(event) {
     if (event.target !== event.currentTarget) {
@@ -14,7 +48,6 @@ function onMissclickPopup(event) {
     }
     popupToggle();
 }
-
 function formSubmitHandler(evt) {
     evt.preventDefault();
     let nameInput = document.querySelector('.popup__input-name');
@@ -29,8 +62,11 @@ function formSubmitHandler(evt) {
     popupToggle();
 }
 
+form.addEventListener('submit', formSubmitHandler);
 popup.addEventListener('click', onMissclickPopup);
 buttonOpenPopup.addEventListener('click', popupToggle);
 buttonClosePopup.addEventListener('click', popupToggle);
-buttonClosePopup.event.stopPropagation();
-form.addEventListener('submit', formSubmitHandler);
+buttonClosePopup.onclick = evt.stopPropagation(); */
+
+
+
