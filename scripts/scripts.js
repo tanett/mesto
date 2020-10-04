b2218c37fa98d73c51869614ba574eff54ca1038
Maxim <samai2@yandex.ru>
2020-10-04 20:09:55 +0300

    исправления по 1 ревью

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,8 +2,15 @@ const buttonOpenPopup = document.querySelector('.profile__edit-button');
 const buttonClosePopup = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 const form = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_about');
+let profileName = document.querySelector('.profile__name');
+let profileDescr = document.querySelector('.profile__descr');
+
 
 function popupOpen() {
+     nameInput.value =  profileName.textContent;
+     jobInput.value = profileDescr.textContent ;
     popup.classList.add('popup_opened');
 }
 
@@ -20,17 +27,13 @@ function onMissclickPopup(event) {
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector('.popup__input-name');
-    let jobInput = document.querySelector('.popup__input-about');
     console.log(nameInput.value);
     console.log(jobInput.value);
-    let profileName = document.querySelector('.profile__name');
-    let profileDescr = document.querySelector('.profile__descr');
-
     profileName.textContent = nameInput.value;
     profileDescr.textContent = jobInput.value;
     popupClose();
 }
+
 form.addEventListener('submit', formSubmitHandler);
 popup.addEventListener('click', onMissclickPopup);
 buttonOpenPopup.addEventListener('click', popupOpen);
